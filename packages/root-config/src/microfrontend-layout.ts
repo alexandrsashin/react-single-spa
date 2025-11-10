9e7a9daec590bd480d837bb80c7319cae6f03caa
Alexandr Sashin <alexandrsashin@gmail.com>
2025-11-10 22:39:26 +0300

    Removed comments from microfrontendLayout

--- packages/root-config/src/microfrontend-layout.ts
+++ packages/root-config/src/microfrontend-layout.ts
@@ -1,12 +1,4 @@
 export const microfrontendLayout = `<single-spa-router>
-  <!--
-    Маршрутизация для микрофронтендов:
-    /login - первый микрофронтенд (форма авторизации)
-    /user - второй микрофронтенд (пользовательский интерфейс)
-    header - отображается на маршрутах /, /user
-    default - редирект на /login если не авторизован, иначе на /user
-  -->
-
   <route path="user">
     <application name="@react-single-spa/header"></application>
     <application name="@react-single-spa/microfrontend2"></application>
@@ -18,6 +10,5 @@ export const microfrontendLayout = `<single-spa-router>
   
   <route default>
     <application name="@react-single-spa/header"></application>
-    <!-- Этот роут будет обрабатываться логикой редиректа -->
   </route>
 </single-spa-router>`;
