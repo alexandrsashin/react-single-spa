ce1fea135a50aa2194227f76e7cce10c1ea6eebf
Alexandr Sashin <alexandrsashin@gmail.com>
2025-11-11 21:00:37 +0300

    Optimized root config layout

--- packages/root-config/src/microfrontend-layout.ts
+++ packages/root-config/src/microfrontend-layout.ts
@@ -1,27 +1,27 @@
 export const microfrontendLayout = `<single-spa-router>
-  <route path="user">
-    <div class="root-layout">
-      <div class="left-col">
-        <application name="@react-single-spa/sidebar"></application>
-      </div>
-      <div class="right-col">
-        <application name="@react-single-spa/header"></application>
-        <application name="@react-single-spa/microfrontend2"></application>
-      </div>
-    </div>
-  </route>
-  
-  <route path="login">
+   <route path="login">
     <application name="@react-single-spa/microfrontend"></application>
   </route>
-  
+
   <route default>
     <div class="root-layout">
       <div class="left-col">
         <application name="@react-single-spa/sidebar"></application>
       </div>
+
       <div class="right-col">
         <application name="@react-single-spa/header"></application>
+
+        <route path="user">
+          <application name="@react-single-spa/microfrontend2"></application>
+        </route>
+
+        <route path="roles">
+          <application name="@react-single-spa/microfrontend2"></application>
+        </route>
+
+        <route default>
+        </route>
       </div>
     </div>
   </route>
